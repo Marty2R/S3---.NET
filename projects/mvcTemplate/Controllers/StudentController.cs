842bbcd3382c9990f94d6952290f23bae6f956c4
Marty Rabatel <marty.rabatel2@gmail.com>
2024-12-10 08:41:02 +0100

    ajout tp student et teacher

--- projects/mvcTemplate/Controllers/StudentController.cs
+++ projects/mvcTemplate/Controllers/StudentController.cs
@@ -14,6 +14,11 @@ namespace mvc.Controllers
             new() { AdmissionDate = new DateTime(2023, 1, 15), Age = 21, FirstName = "Bob", GPA = 3.2, Id = 2, LastName = "Johnson", Major = Major.IT },
             new() { AdmissionDate = new DateTime(2022, 8, 30), Age = 20, FirstName = "Emma", GPA = 3.9, Id = 3, LastName = "Williams", Major = Major.MATH },
             new() { AdmissionDate = new DateTime(2023, 9, 1), Age = 18, FirstName = "Michael", GPA = 3.4, Id = 4, LastName = "Brown", Major = Major.OTHER },
+            new() { AdmissionDate = new DateTime(2023, 9, 1), Age = 18, FirstName = "Michael", GPA = 3.4, Id = 5, LastName = "Brown", Major = Major.OTHER },
+            new() { AdmissionDate = new DateTime(2023, 9, 1), Age = 18, FirstName = "Michael", GPA = 3.4, Id = 6, LastName = "Brown", Major = Major.OTHER },
+            new() { AdmissionDate = new DateTime(2023, 9, 1), Age = 18, FirstName = "Michael", GPA = 3.4, Id = 7, LastName = "Brown", Major = Major.OTHER },
+            new() { AdmissionDate = new DateTime(2023, 9, 1), Age = 18, FirstName = "Michael", GPA = 3.4, Id = 8, LastName = "Brown", Major = Major.OTHER },
+            new() { AdmissionDate = new DateTime(2023, 9, 1), Age = 18, FirstName = "Michael", GPA = 3.4, Id = 9, LastName = "Brown", Major = Major.OTHER },
         };
 
         public ActionResult Delete(int id)
